cbd25ad665b3425086300034f4189efb34322549
php-mainacad <tsubar.v@gmail.com>
2023-06-12 19:18:23 +0300

    Val_1206

---
+++ SMS/SMS/IMesageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SMS
+{
+    [ServiceContract]
+    public interface IMesageService
+    {
+        [OperationContract]
+        string GetMessage(string message);
+    }
+}
