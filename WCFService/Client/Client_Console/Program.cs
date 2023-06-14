f0e7b055ff61ed6b4d3d84e01e9dde9169e0cab2
Valentyn <76053058+Valentyn-TS@users.noreply.github.com>
2023-06-14 16:24:37 +0300

    Sucsess!! ))

--- WCFService/Client/Client_Console/Program.cs
+++ WCFService/Client/Client_Console/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ServiceReference1;
+using System.ServiceModel;
+
+Service1Client client = new Service1Client();
+
+try
+{
+    int result = client.SquareAsync(5).Result;
+    Console.WriteLine("Результат: " + result);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Помилка: " + ex.Message);
+}
+finally
+{
+    client.Close();
+}
+
+Console.ReadLine();
+
