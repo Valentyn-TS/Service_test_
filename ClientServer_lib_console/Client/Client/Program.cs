be41b8389b24280135c6fa6b50d719ead486bd19
Valentyn <76053058+Valentyn-TS@users.noreply.github.com>
2023-06-15 21:29:18 +0300

    Stage 1 Base Without logging

---
+++ ClientServer_lib_console/Client/Client/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("x = ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("y = ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+var endPointConfiguration = MultiplyService.MultiplyServiceClient.EndpointConfiguration.BasicHttpBinding_IMultiplyService;
+var multiplyClient = new MultiplyService.MultiplyServiceClient(endPointConfiguration);
+
+int result = multiplyClient.MultiplyAsync(x, y).Result;
+Console.WriteLine("{0} * {1} = {2}", x, y, result);
---
+++ ClientServer_lib_console/Host/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(Server.MultiplyService)))
+            {
+                host.Open();
+
+                Console.WriteLine("Host started....");
+                Console.ReadLine();
+
+                host.Close();
+            };
+        }
+    }
+}
