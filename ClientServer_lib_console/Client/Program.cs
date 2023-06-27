56b0843a5c65f5b9b1f39876977851ad3e031982
Valentyn <76053058+Valentyn-TS@users.noreply.github.com>
2023-06-27 23:41:17 +0300

    2

--- ClientServer_lib_console/Client/Program.cs
+++ ClientServer_lib_console/Client/Program.cs
@@ -5,14 +5,34 @@ using System;
 using TSMService;
 
 
+WSHttpBinding b = new WSHttpBinding();
+b.Security.Mode = SecurityMode.TransportWithMessageCredential;
+b.Security.Message.ClientCredentialType = MessageCredentialType.Certificate;
 
+Uri httpsAddress = new Uri("https://local:44318/");
 
+//ServiceHost sh = new ServiceHost(typeof(Calculator), httpsAddress);
+//sh.AddServiceEndpoint(typeof(ICalculator), b, "HttpsCalculator");
+//sh.Open();
+//Console.WriteLine("Listening");
+//Console.ReadLine();
 
-TSMServiceClient tSMServiceClient = new TSMServiceClient();
+EndpointAddress ea = new EndpointAddress(httpsAddress);
+TSMServiceClient tSMServiceClient = new TSMServiceClient(b, ea);
+
+string currentDirectory = Directory.GetCurrentDirectory();
+
+string certDirectoryPath = @"D:\Repos\Service_test_\ClientServer_lib_console\Client\cert\client.pfx";
+
+X509Certificate2 cert = new X509Certificate2(certDirectoryPath, "");
+
+tSMServiceClient.ClientCredentials.ClientCertificate.Certificate = cert;
+
+tSMServiceClient.Open(); 
 
 string result = await tSMServiceClient.DoWorkAsync();
-Console.WriteLine(result);
 
+Console.WriteLine(result);
 Console.ReadKey();
 
 tSMServiceClient.Close();   
