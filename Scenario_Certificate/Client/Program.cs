305d4764ed34a6b7f84e61061cc97b8549f9d80d
Valentyn <76053058+Valentyn-TS@users.noreply.github.com>
2023-07-03 13:29:11 +0300

    SSL certs

--- Scenario_Certificate/Client/Program.cs
+++ Scenario_Certificate/Client/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using MyService;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+
+
+WSHttpBinding wSHttpBinding = new WSHttpBinding();
+wSHttpBinding.Security.Mode = SecurityMode.Transport;
+wSHttpBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
+
+Uri httpsUri = new Uri("https://localhost:44305");
+
+string certDirectoryPath = @"D:\Repos\Service_test_\ClientServer_lib_console\Client\cert\client.pfx";
+
+X509Certificate2 cert = new X509Certificate2(certDirectoryPath, "");
+
+MyServiceClient client = new MyServiceClient(wSHttpBinding, new EndpointAddress(httpsUri));
+
+client.ClientCredentials.ClientCertificate.Certificate = cert;
+
+//client.ClientCredentials.ClientCertificate.SetCertificate(
+//    StoreLocation.CurrentUser,
+//    StoreName.My,
+//    X509FindType.FindBySubjectName,
+//    "client");
+
+client.Open();
+
+string result = await client.DoWorkAsync(); 
+
+Console.WriteLine(result);
+Console.ReadKey();
+
+client.Close(); 
