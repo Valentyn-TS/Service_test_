84d2694b6198f6798799843b2d60dd26b988c8a9
Valentyn <76053058+Valentyn-TS@users.noreply.github.com>
2023-07-05 18:30:58 +0300

    1

---
+++ Scenario_Message_UserName/ClientApp/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ServiceModel;
+using ServiceReference1;
+
+WSHttpBinding myBinding = new WSHttpBinding();
+myBinding.Security.Mode = SecurityMode.Message;
+myBinding.Security.Message.ClientCredentialType = MessageCredentialType.UserName;
+
+EndpointAddress ea = new
+    EndpointAddress("http://localhost:14209/MyService");
+
+ MyServiceClient client = new MyServiceClient(myBinding, ea);
+
+client.ClientCredentials.UserName.UserName = "Vall";
+client.ClientCredentials.UserName.Password = "test_009";
+
+client.Open();
+
+string result;
+result = await client.DoWorkAsync();    
+    
+Console.WriteLine(result);
+Console.ReadKey();
+
+client.Close();
+
+
