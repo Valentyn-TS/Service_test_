3f66d5e8f42f5b6f237a05a9afaaecfc097e5413
Valentyn <76053058+Valentyn-TS@users.noreply.github.com>
2023-07-05 18:40:23 +0300

    2

---
+++ Scenario_Message_UserName/ClientAppNet472/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ClientAppNet472.ServiceReference1;
+
+namespace ClientAppNet472
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            WSHttpBinding myBinding = new WSHttpBinding();
+            myBinding.Security.Mode = SecurityMode.Message;
+            myBinding.Security.Message.ClientCredentialType = MessageCredentialType.UserName;
+
+            EndpointAddress ea = new
+                EndpointAddress("http://localhost:14209/");
+
+            MyServiceClient client = new MyServiceClient(myBinding, ea);
+
+            client.ClientCredentials.UserName.UserName = "Vall";
+            client.ClientCredentials.UserName.Password = "test_009";
+
+            client.Open();
+
+            string result;
+            result = await client.DoWorkAsync();
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+            client.Close();
+
+
+        }
+    }
+}
