8748ebdc6d7f166d551f33197376eb98acc36d7f
Valentyn <76053058+Valentyn-TS@users.noreply.github.com>
2023-07-06 15:41:35 +0300

    cert

--- Scenario_Message_Sertificate/ClientCert/Program.cs
+++ Scenario_Message_Sertificate/ClientCert/Program.cs
@@ -1,8 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using ServiceReference1;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel.Channels;
 
 MyServiceClient client = new MyServiceClient();
+
+//client.ClientCredentials.ClientCertificate.SetCertificate(
+//    StoreLocation.CurrentUser,
+//    StoreName.My,
+//    X509FindType.FindBySerialNumber,
+//    "4c4ca8ba913257523b51933c1fd8b4adba494681");
+
+X509Certificate2 x509Certificate2 = new X509Certificate2("C:\\Users\\tsuba\\myCA\\to_PFX_client\\client.pfx", "");
+client.ClientCredentials.ClientCertificate.Certificate = x509Certificate2;
+
+Console.WriteLine(typeof(X509Certificate2));
+Console.WriteLine(x509Certificate2);
 
 client.Open(); 
 
