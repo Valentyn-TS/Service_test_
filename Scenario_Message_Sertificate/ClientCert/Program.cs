d2a8e742fa0fcfdbc635698ea0ecb08fdf959dd7
Valentyn <76053058+Valentyn-TS@users.noreply.github.com>
2023-07-06 10:06:05 +0300

    Emmmm

---
+++ Scenario_Message_Sertificate/ClientCert/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ServiceReference1;
+
+MyServiceClient client = new MyServiceClient();
+
+client.Open(); 
+
+string result = await client.DoWorkAsync(); 
+Console.WriteLine(result);
+Console.ReadKey();  
+
+client.Close();
