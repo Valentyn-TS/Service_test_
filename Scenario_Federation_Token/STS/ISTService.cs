62b1036d05bd1c96af8cf366e09ea768ed869e5f
Valentyn-TS <tsubar.v@gmail.com>
2023-07-12 16:22:58 +0300

    1

--- Scenario_Federation_Token/STS/ISTService.cs
+++ Scenario_Federation_Token/STS/ISTService.cs
@@ -7,9 +7,9 @@ using System.Text;
 
 namespace STS
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISTService" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface ISTService
     {
         [OperationContract]
         void DoWork();
---
+++ Scenario_Federation_Token/STS/STService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace STS
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "STService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select STService.svc or STService.svc.cs at the Solution Explorer and start debugging.
+    public class STService : ISTService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
