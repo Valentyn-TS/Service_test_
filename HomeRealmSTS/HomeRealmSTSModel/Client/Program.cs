2c146730a4e53b387fd187f9df3aaefc63446054
Valentyn-TS <tsubar.v@gmail.com>
2023-07-21 13:27:20 +0300

    Add Issuer in Federation cfg

--- HomeRealmSTS/HomeRealmSTSModel/Client/Program.cs
+++ HomeRealmSTS/HomeRealmSTSModel/Client/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Client
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            MyServiceR.MyServiceClient client = new MyServiceR.MyServiceClient();
+
+            Console.WriteLine(client.DoWorkAsync().Result);
+            Console.ReadLine();
         }
     }
 }
