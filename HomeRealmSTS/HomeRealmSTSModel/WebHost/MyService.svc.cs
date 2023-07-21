e7e57fe1400d97f19f28cc44cd96b432c87b255b
Valentyn-TS <tsubar.v@gmail.com>
2023-07-21 13:44:42 +0300

    Add ServiceAutorizationManager to MyService

--- HomeRealmSTS/HomeRealmSTSModel/WebHost/MyService.svc.cs
+++ HomeRealmSTS/HomeRealmSTSModel/WebHost/MyService.svc.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Claims;
+using System.IdentityModel.Policy;
+using System.IdentityModel.Tokens;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;
 using System.Text;
+
+
 
 namespace WebHost
 {
@@ -16,4 +22,52 @@ namespace WebHost
             return "Hello =)";
         }
     }
+
+    public class myServiceAuthorizationManager : ServiceAuthorizationManager
+    {
+        public override bool CheckAccess(OperationContext operationContext)
+        {
+            AuthorizationContext authContext = operationContext.ServiceSecurityContext.AuthorizationContext;
+            if (authContext.ClaimSets == null) return false;
+            if (authContext.ClaimSets.Count != 1) return false;
+            ClaimSet myClaimSet = authContext.ClaimSets[0];
+            if (!IssuedByHomeSTS(myClaimSet)) return false;
+            if (myClaimSet.Count != 1) return false;
+            Claim myClaim = myClaimSet[0];
+            if (myClaim.ClaimType == "http://www.tmpuri.org:accessAuthorized")
+            {
+                string resource = myClaim.Resource as string;
+                if (resource == null) return false;
+                if (resource != "true") return false;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool IssuedByHomeSTS(ClaimSet myClaimSet)
+        {
+            ClaimSet issuerClaimSet = myClaimSet.Issuer;
+            if (issuerClaimSet == null) return false;
+            if (issuerClaimSet.Count != 1) return false;
+            Claim issuerClaim = issuerClaimSet[0];
+            if (issuerClaim.ClaimType != ClaimTypes.Thumbprint)
+                return false;
+            if (issuerClaim.Resource == null) return false;
+            byte[] claimThumbprint = (byte[])issuerClaim.Resource;
+
+            X509Certificate2 homeSts_Certificate = GetHomeStsCertificate();
+            byte[] certThumbprint = homeSts_Certificate.GetCertHash();
+            if (claimThumbprint.Length != certThumbprint.Length)
+                return false;
+            for (int i = 0; i < claimThumbprint.Length; i++)
+            {
+                if (claimThumbprint[i] != certThumbprint[i]) return false;
+            }
+            return true;
+        }
+    }
+
 }
