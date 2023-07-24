bf65055f0c3f52240739ce0985883facb7e3cb16
Valentyn-TS <tsubar.v@gmail.com>
2023-07-24 12:16:14 +0300

    configuring of client

--- HomeRealmSTS/HomeRealmSTSModel/Client/Program.cs
+++ HomeRealmSTS/HomeRealmSTSModel/Client/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +14,25 @@ namespace Client
 {
     internal class Program
     {
+        private static object cert;
+
         static void Main(string[] args)
         {
-            MyServiceR.MyServiceClient client = new MyServiceR.MyServiceClient();
+            WSFederationHttpBinding b = new WSFederationHttpBinding();
+            b.Security.Mode = WSFederationHttpSecurityMode.Message;
+            b.Security.Message.IssuedTokenType = "http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV1.1";
+            EndpointAddress issuerEA = new EndpointAddress("http://localhost:50987/HomeSTS/MySTS.svc");
+            b.Security.Message.IssuerAddress = issuerEA;
+            EndpointAddress issuerMetadataEA = new EndpointAddress("http://localhost:50987/HomeSTS/MySTS.svc/mex");
+            b.Security.Message.IssuerMetadataAddress = issuerMetadataEA;
+            
+            EndpointAddress serviceEA = new EndpointAddress("http://localhost:50500//MyService/MyService.svc");
+
+            MyServiceR.MyServiceClient client = new MyServiceR.MyServiceClient(b, serviceEA);
+
+            //client.ClientCredentials.ClientCertificate.Certificate; 
+            X509Certificate2 cert = new X509Certificate2();
+            //    cert = X509Certificate2(StoreLocation.CurrentUser, StoreName.My, X509FindType.FindBySerialNumber, "");
 
             Console.WriteLine(client.DoWorkAsync().Result);
             Console.ReadLine();
