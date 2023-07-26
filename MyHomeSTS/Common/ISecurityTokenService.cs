4aa9ae114d336005e645e01a85bf5649e11d3b39
Valentyn <tsubar.v@gmail.com>
2023-07-26 15:51:21 +0300

    Creating... =)

---
+++ MyHomeSTS/Common/ISecurityTokenService.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel.Channels;
+using System.ServiceModel;
+
+namespace My_STS
+{
+    [ServiceContract(Name = "SecurityTokenService", Namespace = "http://tempuri.org")]
+    public interface ISecurityTokenService
+    {
+        [OperationContract(Action = Constants.Trust.Actions.Issue,
+                           ReplyAction = Constants.Trust.Actions.IssueReply)]
+        Message ProcessRequestSecurityToken(Message message);
+    }
+}
