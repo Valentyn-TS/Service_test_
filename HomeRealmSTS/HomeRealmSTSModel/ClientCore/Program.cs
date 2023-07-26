fdbb1eee6c7569b28473110e53656308e4c99baf
Valentyn-TS <tsubar.v@gmail.com>
2023-07-26 09:27:35 +0300

    123

--- HomeRealmSTS/HomeRealmSTSModel/ClientCore/Program.cs
+++ HomeRealmSTS/HomeRealmSTSModel/ClientCore/Program.cs
@@ -5,6 +5,8 @@ using System.ServiceModel;
 using MyService;
 using Microsoft.IdentityModel.Protocols.WsTrust;
 using System.Linq;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
 
 namespace ClientCore
 {
@@ -18,21 +20,30 @@ namespace ClientCore
             WSTrustTokenParameters trustTokenParams = new WSTrustTokenParameters();
             
             trustTokenParams.IssuerAddress = new EndpointAddress("http://localhost:50987/HomeSTS/MySTS.svc");
-            trustTokenParams.IssuerBinding = new BasicHttpBinding();
+            trustTokenParams.IssuerBinding = new WSHttpBinding();
             trustTokenParams.TokenType = "http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV1.1";
+            // trustTokenParams.MessageSecurityVersion.SecurityVersion = "http://schemas.xmlsoap.org/soap/envelope/";
 
+            //Console.WriteLine(trustTokenParams.MessageSecurityVersion.SecurityPolicyVersion.ToString());
+            //Console.WriteLine(trustTokenParams.MessageSecurityVersion.SecurityVersion.ToString());
+            
             WSFederationHttpBinding b = new WSFederationHttpBinding(trustTokenParams);
             b.Security.Mode =  SecurityMode.Message;
-            b.MessageVersion = "http://schemas.xmlsoap.org/soap/envelope/";
-
-
 
             //            EndpointAddress issuerMetadataEA = new EndpointAddress("http://localhost:50987/HomeSTS/MySTS.svc/mex");
             //            b.Security.Message.IssuerMetadataAddress = issuerMetadataEA;
 
             EndpointAddress serviceEA = new EndpointAddress("http://localhost:50500//MyService/MyService.svc");
 
+            // Diagnostic section
+
+            //ActivitySource activitySource = new ActivitySource("message");
+            //ActivityListener activityListener = new ActivityListener();
+            //XmlWriterTraceListener traceListener = new XmlWriterTraceListener("message");
+            
             MyServiceClient client = new MyServiceClient(b, serviceEA);
+
+
 
             client.ClientCredentials.ClientCertificate.SetCertificate(
                 StoreLocation.CurrentUser,
@@ -40,10 +51,16 @@ namespace ClientCore
                 X509FindType.FindBySerialNumber,
                 "17c0c8e126287414ef07bb5e76d9a208aececde1");
 
-            Console.WriteLine(client.DoWorkAsync().Result);
+            //Console.WriteLine(client.DoWorkAsync().Result);
+
+            Console.WriteLine(client.Endpoint.ToString());
+            Console.WriteLine(client.ClientCredentials.ClientCertificate.ToString());   
+            Console.WriteLine(client.OpenAsync());
             Console.ReadLine();
 
-            client.Close();
+            Console.WriteLine(client.DoWorkAsync());
+            Console.ReadLine();
+
         }
     }
 }
