18d0eb333bf40ff0f86810700e400bf2f8e50b4c
Valentyn <tsubar.v@gmail.com>
2023-07-28 11:46:50 +0300

    2

---
+++ MyHomeSTS/CalculatorServiceSTS/CalculatorServiceSTSHost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_STS
+{
+    class CalculatorServiceSTSHostFactory : ServiceHostFactoryBase
+    {
+        public override ServiceHostBase CreateServiceHost(string constructorString, Uri[] baseAddresses)
+        {
+            return new CalculatorServiceSTSHost(baseAddresses);
+        }
+    }
+
+    class CalculatorServiceSTSHost : ServiceHost
+    {
+        public CalculatorServiceSTSHost(params Uri[] addresses) : base(typeof(CalculatorServiceSTS), addresses)
+        {
+            ServiceConstants.LoadAppSettings();
+            this.Credentials.IssuedTokenAuthentication.CertificateValidationMode = X509CertificateValidationMode.PeerOrChainTrust;
+        }
+    }
+}
---
+++ MyHomeSTS/CalculatorServiceSTS/ServiceConstants.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_STS
+{
+    class ServiceConstants
+    {
+        // Issuer name placed into issued tokens
+        internal const string SecurityTokenServiceName = "CalculatorService STS";
+
+        // Statics for location of certs
+        internal static readonly StoreName CertStoreName = StoreName.TrustedPeople;
+        internal static readonly StoreLocation CertStoreLocation = StoreLocation.LocalMachine;
+
+        // Statics initialized from app.config
+        internal static string CertDistinguishedName;
+        internal static string TargetDistinguishedName;
+        internal static string IssuerDistinguishedName;
+
+        /// <summary>
+        /// Helper function to load Application Settings from config
+        /// </summary>
+        public static void LoadAppSettings()
+        {
+            CertDistinguishedName = ConfigurationManager.AppSettings["certDistinguishedName"];
+            CheckIfLoaded(CertDistinguishedName);
+
+            TargetDistinguishedName = ConfigurationManager.AppSettings["targetDistinguishedName"];
+            CheckIfLoaded(TargetDistinguishedName);
+
+            IssuerDistinguishedName = ConfigurationManager.AppSettings["issuerDistinguishedName"];
+            CheckIfLoaded(IssuerDistinguishedName);
+        }
+
+        /// <summary>
+        /// Helper function to check if a required Application Setting has been specified in config.
+        /// Throw if some Application Setting has not been specified.
+        /// </summary>
+        private static void CheckIfLoaded(string s)
+        {
+            if (String.IsNullOrEmpty(s))
+            {
+                throw new ConfigurationErrorsException("Required Configuration Element(s) missing at BookStoreSTS. Please check the STS configuration file.");
+            }
+        }
+
+        private ServiceConstants() { }
+    }
+}
